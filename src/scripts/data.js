98207b050a6377737f2ba90a17805aba937a0677
maychondouglas <maychondouglas@gmail.com>
2021-06-15 19:00:13 -0300

    busca pelo aluno enadinte (ajustes)

---
+++ src/scripts/data.js
@@ -0,0 +1,23 @@
+export const data = {
+    enadintes : [{
+      matricula: 'abcdefghijk',
+      cpf: '1010101015',
+      nome: 'Outro'
+    },
+    {
+      matricula: '91c4dbb8885d86649a20da87ef57c3f0', //matrícula
+      cpf: '0c8646abdf9cc9426278c0b6f9156f3b', //cpf
+      nome: 'Maria do Bonfim Dutra da Silva'
+    },
+    {
+      matricula: '852e76f2729b6063822debef64e7f497',
+      cpf: '3a1d9d6cabb44386ff7eab2d2cd3e12b',
+      nome: 'Tyrone Pires de Lima'
+    },
+    {
+      matricula: 'd6878325b3fcac4eb46ec0e21f5bc6bd',
+      chave2: 'cbc2981f31053e56cb79596445daf701',
+      nome: 'Maychon Douglas Dutra Pires'
+    }]
+  
+  }
